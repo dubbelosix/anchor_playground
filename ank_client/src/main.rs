f466678f68cced0d89bd1338d4ec7d33d5b3edd5
dubbelosix <dub@06.com>
2022-01-18 10:10:43 +0530

    keypair args

--- ank_client/src/main.rs
+++ ank_client/src/main.rs
@@ -8,6 +8,10 @@ use {
         pubkey::Pubkey,
     },
     anchor_lang::prelude::*,
+    clap::{
+        App,
+        Arg,
+    },
 };
 
 // anchor program imports
@@ -20,10 +24,31 @@ use alloc::rc::Rc;
 extern crate alloc;
 
 fn main() {
-    let kp = read_keypair_file("/Users/rohan/.solw/dub.json").unwrap();
+    let app = App::new("ank_client")
+        .version("0.1.0")
+        .about("using rust anchor client to interact with anchor because fun")
+        .author("dubbelosix")
+        .arg(
+            Arg::new("keypair")
+                .short('k')
+                .long("keypair")
+                .help("path to the json keypair file for paying")
+                .takes_value(true)
+                .required(true)
+        );
+    let matches = app.get_matches();
+    let mut keypair_file: String = String::new();
+    if let Some(i) = matches.value_of("keypair") {
+        keypair_file = i.to_string()
+    };
+    let kp = read_keypair_file(keypair_file).unwrap();
     let c = Client::new(Cluster::Localnet, Rc::new(kp));
     let program_id: Pubkey = Pubkey::new(
         &bs58::decode("DoGiWHsUGypgyxtofEdD3qUYxTTjxewr54eGUqtJQQ4").into_vec().unwrap());
     let prog = c.program(program_id);
-    prog.request().accounts(ank_accounts::Initialize{}).args(ank_instruction::Initialize {}).send();
+    let signature = prog.request()
+                                            .accounts(ank_accounts::Initialize{})
+                                            .args(ank_instruction::Initialize {})
+                                            .send();
+    println!("{:?}",signature);
 }
